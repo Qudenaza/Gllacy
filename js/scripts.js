3e391d97017bdca8bb19f79359afb8766c5e245a
Qudenaza <30824548+Qudenaza@users.noreply.github.com>
2017-09-02 13:27:08 +0300

    Добавил JS, исправил ошибки.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,47 @@
 var link = document.querySelector(".feedback-btn");
 var popup = document.querySelector(".modal-feedback");
 var close = document.querySelector(".modal-close");
-var shadow = document.querySelector(".modal-overlay")
+var shadow = document.querySelector(".modal-overlay");
+var form = popup.querySelector("form");
+var userName = popup.querySelector("[name=user-name]");
+var mail = popup.querySelector("[name=email]");
+var storage = localStorage.getItem("userName");
 
 
 link.addEventListener("click", function (event) {
 	event.preventDefault();
 	popup.classList.add("modal-content-show");
 	shadow.classList.add("modal-overlay-show");
+	if (storage) {
+		userName.value = storage;
+		mail.focus();
+	} else {
+		userName.focus();
+	}
 });
 
 close.addEventListener("click", function (event) {
 	event.preventDefault();
 	popup.classList.remove("modal-content-show");
+	popup.classList.remove("modal-error");
 	shadow.classList.remove("modal-overlay-show");
 });
+
+form.addEventListener("submit", function(event) {
+	if (!userName.value || !mail.value) {
+		event.preventDefault();
+		popup.classList.add("modal-error");
+	} else {
+		localStorage.setItem("userName", userName.value);
+	}
+});
+
 
 window.addEventListener("keydown", function(event){
 	if (event.keyCode === 27) {
 		if (popup.classList.contains("modal-content-show")) {
 		 popup.classList.remove("modal-content-show");
+		 popup.classList.remove("modal-error");
 		 shadow.classList.remove("modal-overlay-show");
 			}
 		}
